a8a6657ce9da753a7d1d0aeb77a6fd7172623ae2
ISatyanarayana <rajuindukuri.2001@gmail.com>
2024-01-30 15:47:01 +0530

    some entitys created

---
+++ src/main/java/com/jobsupport/entity/Client.java
@@ -0,0 +1,24 @@
+package com.jobsupport.entity;
+
+public class Client {
+
+private String firstname;
+	
+	private String lastname;
+	
+	private long phonenumber;
+	
+	private String email;
+	
+	private String password;
+	
+	private String role;
+	
+	private String country;
+	
+	private long experience;
+	
+	private String description;
+	
+	private String domain;
+}
