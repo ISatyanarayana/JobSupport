529cfc54c4f435e5aa3ef7dc16a2af070ad50ba2
ISatyanarayana <147706599+ISatyanarayana@users.noreply.github.com>
2024-01-30 15:47:43 +0530

    Satya (#2)
    * application props updated
    
    * ]
    
    * some entitys created

---
+++ src/main/java/com/jobsupport/dto/Project.java
@@ -0,0 +1,14 @@
+package com.jobsupport.dto;
+
+public class Project {
+
+	private String domain;
+	
+	private String contracttype;
+	
+	private String pname;
+	
+	private String pdescription;
+	
+	
+}
---
+++ src/main/java/com/jobsupport/dto/User.java
@@ -0,0 +1,18 @@
+package com.jobsupport.dto;
+
+public class User {
+	
+	private String firstname;
+	
+	private String lastname;
+	
+	private long phonenumber;
+	
+	private String email;
+	
+	private String password;
+	
+	private String role;
+	
+	private String country;
+}
